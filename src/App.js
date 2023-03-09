2f218d6d2bcc9e901f70cdfaf9387075dd098b6a
LongNd <longnd@sphinxjsc.com>
2023-03-09 11:26:42 +0700

    cm code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,182 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  PointElement,
+  LineElement,
+} from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  PointElement,
+  LineElement
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center gap-[20px]">
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <div className="max-w-[80vw] max-h-[50vh] flex gap-5 flex-wrap justify-between">
+        <div className="w-[45%]">
+          <Bar
+            data={{
+              labels: [
+                "Africa",
+                "Asia",
+                "Europe",
+                "Latin America",
+                "North America",
+              ],
+              datasets: [
+                {
+                  label: "Population (millions)",
+                  backgroundColor: [
+                    "#3e95cd",
+                    "#8e5ea2",
+                    "#3cba9f",
+                    "#e8c3b9",
+                    "#c45850",
+                  ],
+                  data: [2478, 5267, 734, 784, 433],
+                },
+              ],
+            }}
+            options={{
+              indexAxis: "y",
+              legend: { display: false },
+              title: {
+                display: true,
+                text: "Predicted world population (millions) in 2050",
+              },
+            }}
+          />
+        </div>
+        <div className="w-[45%]">
+          <Bar
+            data={{
+              labels: [
+                "Africa",
+                "Asia",
+                "Europe",
+                "Latin America",
+                "North America",
+              ],
+              datasets: [
+                {
+                  label: "Population (millions)",
+                  backgroundColor: [
+                    "#3e95cd",
+                    "#8e5ea2",
+                    "#3cba9f",
+                    "#e8c3b9",
+                    "#c45850",
+                  ],
+                  data: [2478, 5267, 734, 784, 433],
+                },
+              ],
+            }}
+            options={{
+              legend: { display: false },
+              title: {
+                display: true,
+                text: "Predicted world population (millions) in 2050",
+              },
+            }}
+          />
+        </div>
+        <div className="w-[45%] h-[400px]">
+          <Doughnut
+            data={{
+              labels: [
+                "Africa",
+                "Asia",
+                "Europe",
+                "Latin America",
+                "North America",
+              ],
+              datasets: [
+                {
+                  label: "Population (millions)",
+                  backgroundColor: [
+                    "#3e95cd",
+                    "#8e5ea2",
+                    "#3cba9f",
+                    "#e8c3b9",
+                    "#c45850",
+                  ],
+                  data: [2478, 5267, 734, 784, 433],
+                },
+              ],
+            }}
+            option={{
+              title: {
+                display: true,
+                text: "Predicted world population (millions) in 2050",
+              },
+            }}
+          />
+        </div>
+        <div className="w-[45%]">
+          <Line
+            options={{
+              responsive: true,
+              plugins: {
+                legend: {
+                  position: "top",
+                },
+                title: {
+                  display: true,
+                  text: "Chart.js Line Chart",
+                },
+              },
+            }}
+            data={{
+              labels: [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+              ],
+              datasets: [
+                {
+                  label: "Dataset 1",
+                  data: [781,415,421,-168,-3,305,-25],
+                  borderColor: "rgb(255, 99, 132)",
+                  backgroundColor: "rgba(255, 99, 132, 0.5)",
+                },
+                {
+                  label: "Dataset 2",
+                  data: [
+                    -77,-862,736,200,-602,185,-332
+                  ],
+                  borderColor: "rgb(53, 162, 235)",
+                  backgroundColor: "rgba(53, 162, 235, 0.5)",
+                },
+              ],
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
